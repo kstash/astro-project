101ec09c81a86f39b09e9e3889a01f1ec5497fb5
Seungtae Kim <25264932+kstash@users.noreply.github.com>
2024-06-24 16:36:03 +0900

    feat: astro config file

---
+++ astro.config.mjs
@@ -0,0 +1,7 @@
+import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
+export default defineConfig({});
+
+// 이 부분 따라서 해보는중
+// https://docs.astro.build/ko/install-and-setup/#%EC%88%98%EB%8F%99-%EC%84%A4%EC%A0%95:~:text=astro.config.mjs,%EC%B0%B8%EC%A1%B0%EB%A5%BC%20%ED%99%95%EC%9D%B8%ED%95%98%EC%84%B8%EC%9A%94.
